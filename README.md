4e9d569b693c943a7cf46c2e528584d9e8437e8d
Elena Katasonova <katasonova.elena.2018@gmail.com>
2025-07-22 15:23:35 +0100

    Add logic for flat jsons comparison

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/katasonova/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/katasonova/frontend-project-46/actions)
+
+Gendiif basic functionality:
+
+[![asciicast](https://asciinema.org/a/Ffd77VYvE5SFynhF36xfYqzlB.svg)](https://asciinema.org/a/Ffd77VYvE5SFynhF36xfYqzlB)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import parser from "./parser.js";
+import _ from "lodash";
 
 const genDiff = (filepath1, filepath2) => {
-  console.log('filepath1:', parser(filepath1));
-  console.log('filepath2:', parser(filepath2));
+  const obj1 = parser(filepath1);
+  const obj2 = parser(filepath2);
+
+  const keys1 = Object.keys(obj1)
+  const keys2 = Object.keys(obj2)
+
+  const uniqueKeys = new Set([...keys1, ...keys2])
+
+  const sortedUniqueKeys = Array.from(uniqueKeys).sort()
+
+  let result = [];
+
+  for (const key of sortedUniqueKeys) {
+    if (keys1.includes(key) && keys2.includes(key)) {
+      if (obj1[key] === obj2[key]) {
+        result.push(`   ${key}: ${obj1[key]}`)
+      } else {
+        result.push(` - ${key}: ${obj1[key]}`)
+        result.push(` + ${key}: ${obj2[key]}`)
+      }
+    }
+    else if (keys1.includes(key) && !keys2.includes(key)) {
+      result.push(` - ${key}: ${obj1[key]}`)
+    }
+    else if (!keys1.includes(key) && keys2.includes(key)) {
+      result.push(` + ${key}: ${obj2[key]}`)
+    }
+  }
+  console.log(`{\n${result.join('\n')}\n}`)
 }
 
 export default genDiff;
